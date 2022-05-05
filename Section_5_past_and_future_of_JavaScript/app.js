c8643e1970b08ff49d5c9436746d39a6821d2233
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2022-05-05 16:07:19 +0200

    Functions vs methods

--- Section_5_past_and_future_of_JavaScript/app.js
+++ Section_5_past_and_future_of_JavaScript/app.js
@@ -1,16 +1,24 @@
-let name = 'Laurent';
+// let name = 'Max';
 
-if (name === 'Laurent') {
-  let hobbies = ['Sports', 'Cooking'];
-  console.log(hobbies);
-}
+// if (name === 'Max') {
+//   let hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
 
-function greet() {
-  let age = 40;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
-}
+// }
 
-console.log(name, hobbies);
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
 
-greet();
+// console.log(name, hobbies);
+
+// greet();
+'use strict';
+
+const userName = 'Max';
+var undefined = 5;
+
+console.log(userName);
+
