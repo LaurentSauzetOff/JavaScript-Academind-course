cae1fb19b54c60d962ea3c4ef9fe4c1a5f67bcd6
LAURENT SAUZET <laurent.sauzetpro@gmail.com>
2023-11-02 13:29:33 +0100

    Controlling the backdrop

---
+++ Favorite_movies/assets/scripts/app.js
@@ -0,0 +1,25 @@
+const addMovieModal = document.getElementById("add-modal");
+const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", toggleMovieModal);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
