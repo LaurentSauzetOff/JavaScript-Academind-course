ce5647add18b013e87fe3e062bedc5c2c4c0d098
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2023-11-04 18:32:31 +0100

    Section 8 started

---
+++ Section_8_More on arrays/Code/app.js
@@ -0,0 +1,36 @@
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+const moreNumbers = new Array(5, 2);
+console.log(moreNumbers);
+
+const yetMoreNumbers = Array(1, 2);
+console.log(yetMoreNumbers);
+
+const yetMoreNumbers2 = Array.of(1, 2);
+console.log(yetMoreNumbers2);
+
+const soMoreNumbers = Array.from("Hello !");
+console.log(soMoreNumbers);
+
+const listItems = document.querySelectorAll("li");
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
+
+const hobbies = ["Cooking", "Sports"];
+const personalData = [41, "Laurent", { moreDetail: [] }];
+
+const analyticsData = [
+  [1, 1.6],
+  [-5.4, 2.1],
+];
+
+for (const data of analyticsData) {
+  for (const dataPoint of data) {
+    console.log(dataPoint);
+  }
+}
+
+console.log(personalData[1]);
