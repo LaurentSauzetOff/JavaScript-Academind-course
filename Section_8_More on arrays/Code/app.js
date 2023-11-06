c1367ada567fb40e3889bcb412f62b224718ad19
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2023-11-06 19:45:14 +0100

    MaJ concat

--- Section_8_More on arrays/Code/app.js
+++ Section_8_More on arrays/Code/app.js
@@ -1,3 +1,5 @@
+/* const { default: test } = require("node:test");
+
 const numbers = [1, 2, 3, 4, 5];
 console.log(numbers);
 
@@ -41,3 +43,25 @@ hobbies2.unshift("Sports");
 const poppedValue = hobbies2.pop();
 hobbies2.shift();
 console.log(hobbies2);
+
+hobbies2.splice(0, 0, "good food");
+
+const removedElements = hobbies2.splice(-1, 1);
+console.log(removedElements);
+console.log(hobbies2);
+
+const results = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const storedResults = results.slice(0, 2);
+
+results.push(5.91);
+
+console.log(storedResults, results); */
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const storedResults = testResults.concat([3.99, 2]);
+
+testResults.push(5.91);
+
+console.log(storedResults, testResults);
+
+
